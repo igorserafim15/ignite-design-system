0aed302eb391efe50d5d93a4568fe9a0d302ed29
Igor Serafim <igor.serafim@nolifretes.com.br>
2023-10-30 12:54:05 -0300

    add tailwind css

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -5,6 +5,9 @@ import { Button } from '@ignite-ui/react'
 export default {
   title: 'Button',
   component: Button,
+  args: {
+    children: 'Clique aqui',
+  },
 } as Meta
 
 export const Primary: StoryObj = {}
